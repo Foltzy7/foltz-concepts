345685fa825e86f24e5d6dac43aefb458494f5a3
Zach Foltz <zfoltzy7@gmail.com>
2022-01-03 01:12:57 -0600

    spacing on main page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Footer from "./components/footer";
 import { RecoilRoot } from "recoil";
 import ErrorBoundary from "./pages/errors/error-boundary";
 import { BrowserRouter } from "react-router-dom";
-import { Box, Flex, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Container, Flex, Image, Text } from "@chakra-ui/react";
 
 function App() {
   return (
@@ -15,32 +15,33 @@ function App() {
           <div className="App">
             <Nav />
             <header className="App-header">
-              <SimpleGrid columns={2} spacing={10}>
-                <Box p={4}>
-                  <Image
-                    src={process.env.PUBLIC_URL + "/selfie.jpg"}
-                    alt={"Zach Foltz"}
-                  />
-                </Box>
-                <Box p={4} h={"100%"}>
-                  <Flex
-                    direction={"column"}
-                    justify={"space-evenly"}
-                    h={"100%"}
-                  >
-                    <Box>
-                      <Text fontSize={"1.25em"}>
-                        Hero Identity: Father of 3
-                      </Text>
-                    </Box>
-                    <Box>
-                      <Text fontSize={"1.25em"}>
-                        Secret Identity: Software Engineer
-                      </Text>
-                    </Box>
-                  </Flex>
-                </Box>
-              </SimpleGrid>
+              <Container width={"100%"}>
+                <Flex direction={"row"} justify={"space-around"}>
+                  <Box p={4}>
+                    <Image
+                      src={process.env.PUBLIC_URL + "/selfie.jpg"}
+                      alt={"Zach Foltz"}
+                      maxH={"350px"}
+                    />
+                  </Box>
+                  <Box p={4}>
+                    <Flex
+                      direction={"column"}
+                      justify={"space-evenly"}
+                      h={"100%"}
+                    >
+                      <Box>
+                        <Text fontSize={"1em"}>Hero Identity:</Text>
+                        <Text fontSize={"1em"}>Husband & Father of 3</Text>
+                      </Box>
+                      <Box>
+                        <Text fontSize={"1em"}>Secret Identity:</Text>
+                        <Text fontSize={"1em"}>Programmer Extraordinaire</Text>
+                      </Box>
+                    </Flex>
+                  </Box>
+                </Flex>
+              </Container>
             </header>
             <Footer />
           </div>
